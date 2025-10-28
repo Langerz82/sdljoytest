2daaf52122303197bfb6502a73280cfa65fe0524
Shantigilbert <shantic@gmail.com>
2025-10-28 01:25:36 -0600

    update `gamepad_info` with `-more` argument
    `gamepad_info -more` will display more information about connected gamepads

--- gamepad_info.cpp
+++ gamepad_info.cpp
@@ -7,133 +7,138 @@
  * (c) Wintermute0110 <wintermute0110@gmail.com> December 2014
  */
 #include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 SDL_Joystick *joy = NULL;
-// Game controllers do not have hats or balls, only joysticks have.
-int SDL_joystick_has_hat = 0;
-
 SDL_GameController *gamepad = NULL;
-SDL_JoystickID instanceID = -1; // Joystick instance ID. Changes if there are hotplug events!!!
-int device_index_in_use = -1; // This is the devic number in use
-int SDL_joystick_is_gamepad = 0; // True if joystick is a SDL2 recognised gamepad
 
-int main(int argn, char** argv)
+int SDL_joystick_has_hat = 0;
+SDL_JoystickID instanceID = -1;
+int device_index_in_use = -1;
+int SDL_joystick_is_gamepad = 0;
+
+// Read UDEV name from /sys/class/input/jsX/device/name
+static void get_udev_name(int index, char *buf, size_t bufsize) {
+    char path[128];
+    snprintf(path, sizeof(path), "/sys/class/input/js%d/device/name", index);
+    FILE *f = fopen(path, "r");
+    if (!f) {
+        snprintf(buf, bufsize, "(unavailable)");
+        return;
+    }
+    if (!fgets(buf, bufsize, f)) {
+        snprintf(buf, bufsize, "(unknown)");
+        fclose(f);
+        return;
+    }
+    buf[strcspn(buf, "\n")] = '\0';
+    fclose(f);
+}
+
+int main(int argn, char **argv)
 {
-    int numJoysticks, i;
     bool moreinfo = false;
 
     if (argn > 1 && strncmp(argv[1], "-more", 10) == 0) {
-      moreinfo = true;
+        moreinfo = true;
     }
-    
-    //
-    // Joystick initialisation
-    // In SDL2, and contrary to what happens in SDL1, joystick events are
-    // received even if video is not initialisated.
-    // Very interesting. If video is initialised in SDL2 then only button 
-    // UP events work! (Tested with a Logitech F710 wireless)!!!
-    //
-    // printf( "Sys_InitInput: SDL2 joystick subsystem init\n" );
+
+    // Init SDL subsystems
     if (SDL_Init(SDL_INIT_JOYSTICK | SDL_INIT_GAMECONTROLLER | SDL_INIT_HAPTIC)) {
-        printf( "Sys_InitInput: SDL_Init() failed: %s\n", SDL_GetError());
+        printf("SDL_Init() failed: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // Load custom gamecontrollerdb file if environment variable is set
+    const char *db_file = SDL_getenv("SDL_GAMECONTROLLERCONFIG_FILE");
+    if (db_file) {
+        if (moreinfo)
+            printf("Loading mappings from %s\n", db_file);
+        SDL_GameControllerAddMappingsFromFile(db_file);
+    }
+
+    int numJoysticks = SDL_NumJoysticks();
+    if (numJoysticks <= 0) {
+        printf("No joystick/gamepad detected.\n");
+        SDL_Quit();
         return 0;
     }
 
-    //
-    // Load controller mappings
-    //
-		const char* db_file = SDL_getenv("SDL_GAMECONTROLLERCONFIG_FILE");
-		if (db_file) {
-			if (moreinfo)
-            printf("Sys_InitInput: Loading %s\n", db_file);
-            int num_devices = SDL_GameControllerAddMappingsFromFile(db_file);
-		}
+    for (int i = 0; i < numJoysticks; i++) {
+        char udev_name[128];
+        get_udev_name(i, udev_name, sizeof(udev_name));
 
-    //
-    // Print joystick information at startup time.
-    //
-    numJoysticks = SDL_NumJoysticks();
-    
-if (numJoysticks > 0)
-{
-    for (i = 0; i < numJoysticks; i++) {
-		int gamepad_idx_to_open = i;
-		
-		// Try to open joystick as controller
-		gamepad = SDL_GameControllerOpen( gamepad_idx_to_open );
-		if( gamepad == NULL ) {
-			SDL_joystick_is_gamepad = 0;
-			// If fails open joystick as joystick (old SDL joystick API)
-			printf("SDL_GameControllerOpen failed: %s\n", SDL_GetError());
-			printf("Opening joystick with old SDL joystick API\n");
-
-			joy = SDL_JoystickOpen( gamepad_idx_to_open );
-			if (joy == NULL ) {
-				printf( "SDL_JoystickOpen failed: %s\n", SDL_GetError() );
-				printf( "Couldn't open joystick %i\n", gamepad_idx_to_open );
-				joy = NULL;
-				instanceID = -1;
-				device_index_in_use = -1;
-			} else {
-				int num_hats;
-				char guid[64];
-
-				instanceID = SDL_JoystickInstanceID(joy);
-				device_index_in_use = gamepad_idx_to_open;
-				SDL_JoystickGetGUIDString(SDL_JoystickGetGUID(joy), guid, sizeof (guid));
-				num_hats = SDL_JoystickNumHats( joy );
-            if (moreinfo) 
-            {
-				printf( "Opened joystick device index %i (%s)\n", 
-								                device_index_in_use, SDL_JoystickNameForIndex( device_index_in_use ) );
-				printf( "        axes: %d\n", SDL_JoystickNumAxes( joy ) );
-				printf( "     buttons: %d\n", SDL_JoystickNumButtons( joy ) );
-				printf( "        hats: %d\n", num_hats );
-				printf( "       balls: %d\n", SDL_JoystickNumBalls( joy ) );
-				printf( " instance id: %d\n", SDL_JoystickInstanceID( joy ) );
-				printf( "        guid: %s\n", guid);
-            } else { 
-            printf("%s%%%s\n", SDL_JoystickName( joy ),guid);
+        // Try to open as gamepad first
+        gamepad = SDL_GameControllerOpen(i);
+        if (gamepad == NULL) {
+            SDL_joystick_is_gamepad = 0;
+            joy = SDL_JoystickOpen(i);
+            if (joy == NULL) {
+                printf("Could not open joystick %d: %s\n", i, SDL_GetError());
+                continue;
             }
-				SDL_joystick_has_hat = 0;
-				if( num_hats )
-					SDL_joystick_has_hat = 1;
-			}
-		} else {
-			SDL_joystick_is_gamepad = 1;
-			joy = SDL_GameControllerGetJoystick( gamepad );
-			instanceID = SDL_JoystickInstanceID(joy);
-			device_index_in_use = gamepad_idx_to_open;
 
-			// Remember that gamepads do not have hats in SDL2
-			int num_axes = 0;
-			int num_buttons = 0;
-			char guid[64];
+            instanceID = SDL_JoystickInstanceID(joy);
+            device_index_in_use = i;
 
-			SDL_JoystickGetGUIDString(SDL_JoystickGetGUID(joy), guid, sizeof (guid));
-            if (moreinfo) 
-            {
-			printf( "Opened gamepad device index %i (%s)\n", 
-							             device_index_in_use, SDL_GameControllerNameForIndex( device_index_in_use ) );
-			printf( "        axes: %d\n", SDL_JoystickNumAxes( joy ) );
-			printf( "        hats: %d\n", SDL_JoystickNumHats( joy ) );
-			printf( "     buttons: %d\n", SDL_JoystickNumButtons( joy ) );
-			printf( " instance id: %d\n", SDL_JoystickInstanceID( joy ) );
-			printf( "        name: %s\n", SDL_JoystickName( joy ) );
-			printf( "        guid: %s\n", guid);
-			printf( "     mapping: %s\n", SDL_GameControllerMapping( gamepad ));
-                } else { 
-                    printf("%s\n", SDL_GameControllerMapping( gamepad ));
+            char guid[64];
+            SDL_JoystickGetGUIDString(SDL_JoystickGetGUID(joy), guid, sizeof(guid));
+            int num_hats = SDL_JoystickNumHats(joy);
+            SDL_joystick_has_hat = num_hats > 0;
+
+            if (moreinfo) {
+                printf("\nJoystick js%d \n", i);
+                printf("UDEV name:       %s\n", udev_name);
+                printf("SDL name:        %s\n", SDL_JoystickName(joy));
+                printf("SDL GUID:        %s\n", guid);
+                printf("Axes:            %d\n", SDL_JoystickNumAxes(joy));
+/*
+                printf("Buttons:         %d\n", SDL_JoystickNumButtons(joy));
+                printf("Hats:            %d\n", num_hats);
+                printf("Balls:           %d\n", SDL_JoystickNumBalls(joy));
+*/
+                printf("Instance ID:     %d\n", instanceID);
+            } else {
+                printf("%s%%%s\n", SDL_JoystickName(joy), guid);
             }
-		}
-	} 
-} else {
-     printf( "No Joystick/Gamepad detected! \n" );
-}
-    //
-    // Shutdown SDL2
-    //
+
+            SDL_JoystickClose(joy);
+            joy = NULL;
+
+        } else {
+            SDL_joystick_is_gamepad = 1;
+            joy = SDL_GameControllerGetJoystick(gamepad);
+            instanceID = SDL_JoystickInstanceID(joy);
+            device_index_in_use = i;
+
+            char guid[64];
+            SDL_JoystickGetGUIDString(SDL_JoystickGetGUID(joy), guid, sizeof(guid));
+            const char *mapping = SDL_GameControllerMapping(gamepad);
+
+            if (moreinfo) {
+                printf("\nGamepad js%d\n", i);
+                printf("UDEV name:       %s\n", udev_name);
+                printf("SDL name:        %s\n", SDL_GameControllerName(gamepad));
+                printf("SDL GUID:        %s\n", guid);
+                printf("Mapping:         %s\n", mapping ? mapping : "(no mapping)");
+/*              printf("Axes:            %d\n", SDL_JoystickNumAxes(joy));
+                printf("Buttons:         %d\n", SDL_JoystickNumButtons(joy));
+                printf("Hats:            %d\n", SDL_JoystickNumHats(joy));
+*/
+                printf("Instance ID:     %d\n", instanceID);
+            } else {
+                printf("%s\n", mapping ? mapping : "(no mapping)");
+            }
+
+            SDL_GameControllerClose(gamepad);
+            gamepad = NULL;
+        }
+    }
+
     SDL_QuitSubSystem(SDL_INIT_JOYSTICK | SDL_INIT_GAMECONTROLLER | SDL_INIT_HAPTIC);
-
+    SDL_Quit();
     return 0;
 }
